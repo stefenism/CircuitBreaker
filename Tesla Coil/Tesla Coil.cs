bd11985dd21ff178c230cf8f5ef80940189c2f94
nickpumper <nickpumper@gmail.com>
2017-01-21 21:56:42 -0600

    Tesla Coil Beginnings

---
+++ Tesla Coil/Tesla Coil.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeslaCoil : MonoBehaviour 
+{
+	/*
+	21 Jan 2017
+	Last Mod: 21 Jan 2017
+	Nicholas Pumper
+
+	WOrks a Tesla Coil, which is constantly managed by GameController.
+	*/
+
+	[Header("Game Instance Variables")]
+	public float 	currentCharge;
+	[Header("Animation Parameters")]
+	public float levelOneMinCharge;
+	public float levelTwoMinCharge;
+	public float levelThreeMinCharge;
+	public float levelFourMinCharge;
+
+	// Private Instance Variables
+	private bool isDead			= false;
+	private bool coilHasStarted = false;
+
+	void Update ()
+	{
+		if (coilHasStarted && !isDead)
+		{
+			CheckIfDead();
+		} // if
+	} // void Update ()
+
+	public void StartCoil()
+	{
+
+	} // public void StartCoil()
+
+	private void CheckIfDead()
+	{
+		
+	} // private void CheckIfDead()
+} // public class TeslaCoil : MonoBehaviour 
